e542f3980f0dd48518a50f38dec2e0c7cd41af23
Anuppun123 <lucky_anup2026@yahoo.com>
2025-04-13 00:32:07 +0200

    Switch class is added

---
+++ src/test/Switch.java
@@ -0,0 +1,34 @@
+package test;
+
+public class Switch {
+    public static void main(String[] args){
+        int number = 9;
+
+        switch (number){
+            case 1:
+            System.out.println("Sunday");
+            break;
+            case 2:
+            System.out.println("Monday");
+            break;
+            case 3:
+            System.out.println("Tuesday");
+            break;
+            case 4:
+            System.out.println("Wednesday");
+            break;
+            case 5:
+            System.out.println("Thrusday");
+            break;
+            case 6:
+            System.out.println("Friday");
+            break;
+            case 7:
+            System.out.println("Saturday");
+            break;
+            default:
+            System.out.println("Sorry,there is only 7 days in a week.");
+        }
+    }
+
+}

1cc57e60e8badab5b3966757b28f2662e0f56115
Anuppun123 <lucky_anup2026@yahoo.com>
2025-04-13 00:13:48 +0200

    information about Math random number

---
+++ src/test/Practice.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Practice {
+    public static void main(String[]args){
+            System.out.println("Minimum number is: "+ Math.min(4,5));
+            System.out.println("Square root of this number is: "+ Math.sqrt(81));
+            System.out.println("Absolut value is: "+ Math.abs(-81));
+            System.out.println("Random number is: "+ Math.random()*100);
+            System.out.println("Random number in whole number is: "+ (int)(Math.random()*100));
+
+
+
+    }
+
+}
